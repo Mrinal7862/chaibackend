cbcb79c066df45ff040ee7e2ec206d165369a34b
Mrinal7862 <codemaxter473@gmail.com>
2025-01-19 21:14:57 +0530

    JWT&VIDEOMODEL

---
+++ src/models/video.model.js
@@ -0,0 +1,42 @@
+import mongoose, {Scheam, Schema} from 'mongoose'
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2'
+
+const videoSchema = new Schema({
+    videoFile:{
+        type: String, //cloundnary
+        required: true,
+
+    },
+    thumbnail:{
+        type: String, //cloudnary
+        required: true
+    },
+    title:{
+        type: String, 
+        required: true
+    },
+    description:{
+        type: String, 
+        required: true
+    },
+    duration:{
+        type: Number, 
+        required: true
+    },
+    views:{
+        type: Number, 
+        default: 0
+    },
+    isPublished:{
+        type: Boolean,
+        default:true
+    },
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+},{timestamp:true})
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model('Video', videoSchema)
